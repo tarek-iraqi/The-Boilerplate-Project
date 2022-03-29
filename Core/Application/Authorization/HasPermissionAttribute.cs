d7af8439858c50776a94cf57a6fba878bcaf8e81
tarek iraqi <tarek.iraqi@ibtikar.net..sa>
2022-03-29 14:04:01 +0200

    change HasPermission Attribute signature for multiple permissions

--- Core/Application/Authorization/HasPermissionAttribute.cs
+++ Core/Application/Authorization/HasPermissionAttribute.cs
@@ -7,14 +7,12 @@ namespace Application.Authorization
     [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, Inherited = false)]
     public class HasPermissionAttribute : AuthorizeAttribute
     {
-
         public HasPermissionAttribute(Permissions permission) : base(permission.ToString())
-        {
-        }
+        {}
 
-        public HasPermissionAttribute(Permissions[] permissions, PermissionCompareOperator compareOperator)
+        public HasPermissionAttribute(PermissionCompareOperator compareOperator, params Permissions[] permissions)
             : base(permissions.Select(p => p.ToString()).Aggregate((a, b) => a + PermissionConstants.PermissionSeparator + b)
                   + PermissionConstants.PermissionCompareOperatorSeparator + compareOperator)
-        { }
+        {}
     }
 }
